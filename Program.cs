ed3b406e8fe89279d669f9ef67981aeeab42026b
johnb08 <johnb08.kodehode@gmail.com>
2025-10-16 14:46:20 +0200

    added comments

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ var jobForJohn = nameAndJobTable["John"];
 
 Console.WriteLine(jobForJohn);
 
+
+/* For å passe på at to dataverdier i to samlinger er linket sammen, har dere kanskje prøvd å bruke to arrays, så synkronisere indexen for hver item." */
 string[] produkter = {
             "Fanta", "Cola", "Cola Zero", "Solo", "Urge",
             "Kjeks", "Lunsj", "Valnøtter", "Hasselnøtter", "Melk"
@@ -21,6 +23,9 @@ int[] priser = {
             20, 30 , 40, 30, 25, // Snacks
         };
 
+
+/* En mer nyttig datatype for dette, ville vært et dictionary. Et dictionary kobler to verdier sammen, i et Key/Value par.
+Hvor du kan bruke nøkkelen for å hente ut verdien.  */
 var produktOgPrisTable = new Dictionary<string, int>()
 {
     ["fanta"] = 25,
@@ -31,6 +36,15 @@ var produktOgPrisTable = new Dictionary<string, int>()
 var prisForProdukt = produktOgPrisTable["cola"];
 
 Console.WriteLine(prisForProdukt);
+
+
+
+/* Et annet triks vi så på, var å synkronisere to samlinger av forskjellig lengde. 
+Vi hadde ett array på lengde 7, og ett array på lengde 3. 
+
+For å garantere i en loop, som loopet over days arrayet også kunne hente et gyldig element i chars arrayet, 
+brukte vi modulo operatoren %.
+Vi kan se for oss at modulo operatoren henter ut rest verdien av en dele operasjon.  se nedenfor for mer info*/
 
 string[] days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
@@ -45,17 +59,31 @@ for (int i = 0; i < days.Length; i++)
 {
     Console.WriteLine(days[i]);
 
+/* Her bruker vi modulo operatoren for å passe på at iteratoren vår, alltid også er en gyldig index av char.
+
+Vi kan se for oss at % betyr følgende:
+    1. ta i og del den på char.length.
+    2. ignorer den hele verdien, men finn resten. 
+        (hvis i er 3 og lengden på chars er 3, er det ingen rest verdi. resultatet er 0, eller første index i chars.)
+        (hvis i er 5 og lengden på chars er 3, har vi en hel 3, og 2 til overs. resultatet er 2, eller tredje index i chars.)
+    */
     var dailyCharacter = chars[i % chars.Length][0];
     Console.WriteLine(dailyCharacter);
 }
 
 Console.WriteLine(Console.ReadLine());
 
+
+/* Her ser vi at datatypene knyttet til dictionaries kan være svært komplisert. Her definerer vi en trupple, med verdier som representerer dager, en greeting og en alder.  */
 var nameDaysGreetingAndAgeDictionary = new Dictionary<string, (string day, string greeting, int age)>()
 {
     ["Mona Lisa"] = ("monday", "What a smile!", 28),
     ["Al Pacino"] = ("tuesday", "Loved the donut commercial!", 85)
 };
+
+/* Vi har mange gode hjelpemetoder for å bruke dictionaries, bl.a. kan vi prøve å bruke en nøkkel, og hente ut en verdi. 
+På samme måte som vi kan bruke TryParse, kan vi bruke TryGetValue for å prøve en nøkkel, og se om det er en verdi der. 
+Vi kan bruke den i programflyt på samme måte som TryParse.  */
 
 Console.WriteLine("Who do you want a greeting for?");
 var input = Console.ReadLine();
